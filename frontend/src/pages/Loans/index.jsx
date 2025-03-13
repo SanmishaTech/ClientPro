b9582acdcf1b3b6d88ac63146f539d07b4c2c8b5
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2025-03-13 18:24:23 +0530

    skeletan updated

--- frontend/src/pages/Loans/index.jsx
+++ frontend/src/pages/Loans/index.jsx
@@ -105,14 +105,43 @@ const Index = () => {
             Add Loan
           </Button>
         </div>
-        {isLoanDataLoading ? (
-          <div className="px-5 mt-8 pb-2 dark:bg-background pt-1 w-full bg-white   rounded-md">
-            <div className="w-full pt-4 pb-6 flex flex-col gap-2 md:flex-row justify-between items-center">
-              <h2 className="text-xl   font-medium leading-none tracking-tight">
-                <Skeleton className="h-6 w-[220px]" />
-              </h2>
-              <Skeleton className="h-6 w-[220px]" />
+
+        <div className="px-5 dark:bg-background pt-1 w-full bg-white shadow-xl border rounded-md">
+          <div className="w-full py-3 flex flex-col gap-2 md:flex-row justify-between items-center">
+            <h2 className="text-2xl font-semibold leading-none tracking-tight">
+              Loans
+            </h2>
+            {/* search field here */}
+            <div className="relative p-0.5 ">
+              <div className="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
+                <svg
+                  className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+              <input
+                type="text"
+                value={search}
+                onChange={(e) => {
+                  setSearch(e.target.value);
+                }}
+                id="search"
+                className="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search for Loans"
+              />
             </div>
+            {/* end */}
+          </div>
+          {isLoanDataLoading ? (
             <Table className="mb-2">
               <TableCaption className="mb-2">
                 <div className="flex justify-end"></div>
@@ -166,43 +195,7 @@ const Index = () => {
                 )}
               </TableBody>
             </Table>
-          </div>
-        ) : (
-          <div className="px-5 dark:bg-background pt-1 w-full bg-white shadow-xl border rounded-md">
-            <div className="w-full py-3 flex flex-col gap-2 md:flex-row justify-between items-center">
-              <h2 className="text-2xl font-semibold leading-none tracking-tight">
-                Loans
-              </h2>
-              {/* search field here */}
-              <div className="relative p-0.5 ">
-                <div className="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
-                  <svg
-                    className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                    aria-hidden="true"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                </div>
-                <input
-                  type="text"
-                  value={search}
-                  onChange={(e) => {
-                    setSearch(e.target.value);
-                  }}
-                  id="search"
-                  className="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Search for Loans"
-                />
-              </div>
-              {/* end */}
-            </div>
+          ) : (
             <Table className="mb-2">
               <TableCaption className="mb-2">
                 <div className="flex justify-end">
@@ -285,8 +278,8 @@ const Index = () => {
                   ))}
               </TableBody>
             </Table>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </>
   );
